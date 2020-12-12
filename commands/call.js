c35604d6b85369977d70918a2eaf21b26d1d1047
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-12 22:32:04 +0100

    fix

--- commands/call.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name : 'call',
-    aliases : ['chat', 'talk', 'scream'],
-    onexecute : (message, args) => {
-        message.client.channels.cache.find(channel => channel.id == 786639730133303357).send('Hey @here, ' + message.author.toString() + " wants to chat! Join a vc to chat with them!");
-    }
-}
--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "prefix":"!",
-    "token":"NzQ4NTgwODE4MjkyODM0NDQ2.X0fgNw.2_MmoBa3cWDHJA46dB_OvwtCrWU",
-    "hornyWords":["niger", "nigger", "niga", "nigga"]
-}
