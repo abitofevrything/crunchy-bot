ca6e8bf4eb210fcea393d4f7fa6e54a9cf684d99
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-14 19:18:51 +0100

    *sigh*

--- commands/decide.js
+++ commands/decide.js
@@ -2,7 +2,7 @@ module.exports = {
     name : "decide",
     aliases : ["choose", "oneof"],
     onexecute : (message, args) => {
-        args = args.join(' ').split('][').map(str => str.split('] [')).flat().map(str => str.startsWith('[') ? str.subString(1) : str).map(str => str.endsWith(']') ? str.subString(-1) : str);
+        args = args.join(' ').split('][').map(str => str.split('] [')).flat().map(str => str.startsWith('[') ? str.substring(1) : str).map(str => str.endsWith(']') ? str.subString(-1) : str);
         let i = Math.floor(Math.random() * args.length);
         message.channel.send(args[i]);
     }
