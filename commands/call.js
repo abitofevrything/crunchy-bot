f34579b7cae7fb7030a70c52a50d5f5a98496ea4
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-14 19:04:49 +0100

    !call now suports users

--- commands/call.js
+++ commands/call.js
@@ -2,11 +2,28 @@ module.exports = {
     name : 'call',
     aliases : ['chat', 'talk', 'scream'],
     onexecute : (message, args) => {
-        let voiceStatus;
-        if ((voiceStatus = message.member.voice).channel != undefined) {
-            message.channel.send('Hey @here, ' + message.author.toString() + ' wants to chat! Join ' + voiceStatus.channel.name + ' to chat with them');
+        if (args.length == 0) {
+            let voiceStatus;
+            if ((voiceStatus = message.member.voice).channel != undefined) {
+                message.channel.send('Hey @here, ' + message.author.toString() + ' wants to chat! Join ' + voiceStatus.channel.name + ' to chat with them');
+            } else {
+                message.channel.send('Hey @here, ' + message.author.toString() + ' wants to chat! Join a vc to chat with them!')
+            }
         } else {
-            message.channel.send('Hey @here, ' + message.author.toString() + ' wants to chat! Join a vc to chat with them!')
+            let users = args[0];
+            for (let i = 1; i < args.length; i++) {
+                if (i == args.length - 1) {
+                    users += ' and ' + args[i];
+                } else {
+                    users += ', ' + args[i];
+                }
+            }
+            let voiceStatus;
+            if ((voiceStatus = message.member.voice).channel != undefined) {
+                message.channel.send('Hey ' + users + ', ' + message.author.toString() + ' wants to chat! Join ' + voiceStatus.channel.name + ' to chat with them');
+            } else {
+                message.channel.send('Hey @' + users + ', ' + message.author.toString() + ' wants to chat! Join a vc to chat with them!')
+            } 
         }
     }
 }
