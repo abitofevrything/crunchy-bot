55c89fd299721a77aef225ab7582d57e7611c4f3
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-14 18:51:36 +0100

    !call now specifies vc if user is connected

--- commands/call.js
+++ commands/call.js
@@ -2,6 +2,11 @@ module.exports = {
     name : 'call',
     aliases : ['chat', 'talk', 'scream'],
     onexecute : (message, args) => {
-        message.client.channels.cache.find(channel => channel.id == 786639730133303357).send('Hey @here, ' + message.author.toString() + " wants to chat! Join a vc to chat with them!");
+        let voiceStatus;
+        if ((voiceStatus = message.member.voice).channel != undefined) {
+            message.channel.send('Hey @here, ' + message.author.toString() + ' wants to chat! Join ' + voiceStatus.channel.name + ' to chat with them');
+        } else {
+            message.channel.send('Hey @here, ' + message.author.toString() + ' wants to chat! Join a vc to chat with them!')
+        }
     }
 }
