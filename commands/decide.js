8030ae1934e57c8d121ef76d4c6319a569765880
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-16 17:33:12 +0100

    added 0-width char to !decide

--- commands/decide.js
+++ commands/decide.js
@@ -4,6 +4,6 @@ module.exports = {
     onexecute : (message, args) => {
         args = args.join(' ').split('][').map(str => str.split('] [')).flat().map(str => str.startsWith('[') ? str.substring(1) : str).map(str => str.endsWith(']') ? str.substring(0, str.length - 1) : str);
         let i = Math.floor(Math.random() * args.length);
-        message.channel.send(args[i]);
+        message.channel.send('​' + args[i]);
     }
 }
