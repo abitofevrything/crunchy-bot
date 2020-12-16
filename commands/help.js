dc2f991b7bd337039ef33d2ac98dd87799e095eb
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-16 18:36:04 +0100

    added !help

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+const { commands } = require('../index.js');
+
+module.exports = {
+    name: "help",
+    aliases: [],
+    onexecute : (message, args) => {
+        let embed = new MessageEmbed().setTitle('Help');
+        embed.addField('Commands', '```' + commands.reduce((acc, curr, index, arr) => acc + curr + (index == arr.length - 1 ? '' : '\n')) + '```');
+
+
+        message.channl.send(embed);
+    }
+}
