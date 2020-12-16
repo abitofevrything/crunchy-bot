1e5064062503a19960f50d6450756032befaf525
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-16 18:42:02 +0100

    fixed !help

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
     aliases: [],
     onexecute : (message, args) => {
         let embed = new MessageEmbed().setTitle('Help');
-        embed.addField('Commands', '```' + commands.reduce((acc, curr, index, arr) => acc + curr + (index == arr.length - 1 ? '' : '\n')) + '```');
+        embed.addField('Commands', '```' + commands().reduce((acc, curr, index, arr) => acc + curr + (index == arr.length - 1 ? '' : '\n')) + '```');
 
 
         message.channl.send(embed);
