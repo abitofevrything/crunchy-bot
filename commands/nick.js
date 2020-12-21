b4b1477986627abf03b32f752531d0a1bfa418f0
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2020-12-21 18:13:16 +0100

    !nick

---
+++ commands/nick.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name : 'nick',
+    aliases : ['n'],
+    help : {
+        desc : 'Sets the nickname of a user',
+        syntax : 'nick (user) (nickname)',
+        perms : 'MANAGE_MEMBERS'
+    },
+    onexecute : (message, args) => {
+        if (!message.member.hasPermission('MANAGE_MEMBERS')) {
+            message.channel.send('Insufficient permissions!');
+            return;
+        }
+
+        let target = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+        if (target.manageable) {
+            target.setNickname(args.splice(1).join(" "));
+        } else {
+            message.channel.send('Cannot edit ' + target.toString() + "'s name!");
+        }
+    }
+
+}
