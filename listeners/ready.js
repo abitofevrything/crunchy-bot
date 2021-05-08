50b9fe5cd828d8f3f23a02f2fa2f0e6b9223f341
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-05-08 18:09:25 +0200

    Add bot status

--- listeners/ready.js
+++ listeners/ready.js
@@ -4,5 +4,14 @@ module.exports = {
     event: 'ready',
     callback: async () => {
         console.log(`Logged in as ${client.user.tag}`);
+
+        client.user.setPresence({
+            status: 'online',
+            activity: {
+                type: 'STREAMING',
+                name: 'your life in 4K',
+                url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            }
+        });
     }
 }
