c7b7182aeeeab59603964e3298859a3128a1e07f
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-05-10 19:57:29 +0200

    Allow alphabet in DMs

--- listeners/alphabet.js
+++ listeners/alphabet.js
@@ -12,11 +12,40 @@ function generateError() {
     }
 }
 
+function process(message) {
+    let key;
+    if (message.guild) key = `${message.guild.id}-${message.channel.id}-${message.author.id}`;
+    else key = message.author.id;
+
+    let content = message.content.toLowerCase().trim();
+    let expected = alphabetData[key] || 'a'.charCodeAt(0);
+
+    let isOneRepeatedCharacter = /^(.)\1*$/g.test(content);
+
+    if (!isOneRepeatedCharacter && expected != 'a'.charCodeAt(0)) {
+        if (content.length <= 3) message.channel.send("**Your streak was broken**\n*You stoopid*");
+        alphabetData[key] = 'a'.charCodeAt(0);
+    } else if (isOneRepeatedCharacter) {
+        if (content.charCodeAt(0) == expected) {
+            if (expected > 'z'.charCodeAt(0)) message.channel.send(generateError());
+            message.channel.send(String.fromCharCode(expected + 1).repeat(content.length));
+            alphabetData[key] = expected + 2;
+        } else {
+            if (expected != 'a'.charCodeAt(0) &&!(expected > 'z'.charCodeAt(0))) {
+                message.channel.send("**Your streak was broken**\n*You stoopid*");
+            }
+            alphabetData[key] = 'a'.charCodeAt(0);
+        }
+    }
+}
+
 module.exports = {
     event: 'message',
     callback: async message => {
         if (!message.content) return;
         if (message.author.bot) return;
+
+        if (!message.guild) return process(message);
 
         db.get(`
                 SELECT alphabet_channels
@@ -29,30 +58,7 @@ module.exports = {
 
                 let enabled = row["alphabet_channels"] == '*' || row["alphabet_channels"].split(',').includes(message.channel.id);
 
-                if (enabled) {
-                    let content = message.content.toLowerCase().trim();
-                    let expected = alphabetData[`${message.guild.id}-${message.channel.id}-${message.author.id}`] || 'a'.charCodeAt(0);
-                
-                    let isOneRepeatedCharacter = /^(.)\1*$/g.test(content);
-
-                    if (!isOneRepeatedCharacter && expected != 'a'.charCodeAt(0)) {
-                        if (content.length <= 3) message.channel.send("**Your streak was broken**\n*You stoopid*");
-                        alphabetData[`${message.guild.id}-${message.channel.id}-${message.author.id}`] = 'a'.charCodeAt(0);
-                    } else if (isOneRepeatedCharacter) {
-                        if (content.charCodeAt(0) == expected) {
-                            if (expected > 'z'.charCodeAt(0)) message.channel.send(generateError());
-                            message.channel.send(String.fromCharCode(expected + 1).repeat(content.length));
-                            alphabetData[`${message.guild.id}-${message.channel.id}-${message.author.id}`] = expected + 2;
-                        } else {
-                            if (expected != 'a'.charCodeAt(0) &&!(expected > 'z'.charCodeAt(0))) {
-                                message.channel.send("**Your streak was broken**\n*You stoopid*");
-                            }
-                            alphabetData[`${message.guild.id}-${message.channel.id}-${message.author.id}`] = 'a'.charCodeAt(0);
-                        }
-                    }
-                }
+                if (enabled) return process(message);
             });
-
-        
     }
 }
