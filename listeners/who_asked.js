2f9fbe89d8e58d6f236cb07c21b5a447603d5930
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-08-18 12:22:18 +0200

    Make who asked case insensitive and fix key error

--- listeners/who_asked.js
+++ listeners/who_asked.js
@@ -1,7 +1,7 @@
 module.exports = {
-    name: 'message',
+    event: 'message',
     callback: async (message) => {
-        if (/who ([^ ]+ ){0,2}asked/g.test(message.content)) {
+        if (/who ([^ ]+ ){0,2}asked/gi.test(message.content)) {
             let msg = await message.channel.send('https://youtu.be/nFrPQ86kxo4');
             msg.suppressEmbeds(true);
         }
