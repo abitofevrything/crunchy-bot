e83678dfcdb79b1ad4472a02366467200ed01579
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-08-18 12:55:28 +0200

    Dont suppress embeds for who asked

--- listeners/who_asked.js
+++ listeners/who_asked.js
@@ -2,8 +2,7 @@ module.exports = {
     event: 'message',
     callback: async (message) => {
         if (/who ([^ ]+ ){0,2}asked/gi.test(message.content)) {
-            let msg = await message.channel.send('https://youtu.be/nFrPQ86kxo4');
-            msg.suppressEmbeds(true);
+            message.channel.send('https://youtu.be/nFrPQ86kxo4');
         }
     }
 }
