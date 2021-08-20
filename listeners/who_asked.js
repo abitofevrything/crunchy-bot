b3a91d0215573a7334fd096f0c69a6777bdbabe0
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-08-20 17:40:40 +0200

    Skip sponsor for who asked

--- listeners/who_asked.js
+++ listeners/who_asked.js
@@ -2,7 +2,7 @@ module.exports = {
     event: 'message',
     callback: async (message) => {
         if (/who ([^ ]+ ){0,2}asked/gi.test(message.content)) {
-            message.channel.send('https://youtu.be/nFrPQ86kxo4');
+            message.channel.send('https://youtu.be/nFrPQ86kxo4?t=66');
         }
     }
 }
